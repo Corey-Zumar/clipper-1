3d2831a25aad51b8ad74ac5768c465fa7224a869
Corey Zumar <czumar@berkeley.edu>
2017-06-28 18:52:47 -0700

    Format code

--- gpu/containers/tf_gpu_inception_container.py
+++ gpu/containers/tf_gpu_inception_container.py
@@ -22,14 +22,17 @@ from datasets import imagenet
 image_size = inception_v3.inception_v3.default_image_size
 slim = tf.contrib.slim
 
-class TfInceptionContainer(rpc.ModelContainerBase):
 
+class TfInceptionContainer(rpc.ModelContainerBase):
     def __init__(self, checkpoint_path, gpu_mem_frac):
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_mem_frac)
+        gpu_options = tf.GPUOptions(
+            per_process_gpu_memory_fraction=gpu_mem_frac)
         self.checkpoint_path = checkpoint_path
-        self.sess = tf.Session("", tf.Graph(), config=tf.ConfigProto(gpu_options=gpu_options))
+        self.sess = tf.Session(
+            "", tf.Graph(), config=tf.ConfigProto(gpu_options=gpu_options))
         with self.sess.graph.as_default():
-            self.inputs = tf.placeholder(tf.float32, (None, image_size, image_size, 3))
+            self.inputs = tf.placeholder(tf.float32, (None, image_size,
+                                                      image_size, 3))
             with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
                 logits, _ = inception_v3.inception_v3(
                     self.inputs, num_classes=1001, is_training=False)
@@ -38,23 +41,29 @@ class TfInceptionContainer(rpc.ModelContainerBase):
                 self.checkpoint_path, slim.get_model_variables("InceptionV3"))
             init_fn(self.sess)
 
-
     def predict_bytes(self, inputs):
         outputs = []
         with self.sess.graph.as_default():
-            decoded_images = [tf.image.decode_jpeg(input_image.tobytes(), channels=3) for input_image in inputs]
+            decoded_images = [
+                tf.image.decode_jpeg(input_image.tobytes(), channels=3)
+                for input_image in inputs
+            ]
             preprocessed_images = [
                 inception_preprocessing.preprocess_image(
-                    decoded_image,
-                    image_size,
-                    image_size,
-                    is_training=False) for decoded_image in decoded_images]
+                    decoded_image, image_size, image_size, is_training=False)
+                for decoded_image in decoded_images
+            ]
 
         processed_inputs = self.sess.run(preprocessed_images)
-        all_probabilities = self.sess.run([self.all_probabilities], feed_dict={self.inputs: processed_inputs})
+        all_probabilities = self.sess.run(
+            [self.all_probabilities],
+            feed_dict={self.inputs: processed_inputs})
         for input_probabilities in all_probabilities[0]:
-            sorted_inds = [i[0] for i in sorted(
-                enumerate(-input_probabilities), key=lambda x:x[1])]
+            sorted_inds = [
+                i[0]
+                for i in sorted(
+                    enumerate(-input_probabilities), key=lambda x: x[1])
+            ]
             outputs.append(str(sorted_inds[0]))
 
         print(outputs)
@@ -87,7 +96,9 @@ if __name__ == "__main__":
     try:
         gpu_mem_frac = os.environ["CLIPPER_GPU_MEM_FRAC"]
     except KeyError:
-        print("ERROR: CLIPPER_GPU_MEM_FRAC environment variable must be set", file=sys.stdout)
+        print(
+            "ERROR: CLIPPER_GPU_MEM_FRAC environment variable must be set",
+            file=sys.stdout)
         sys.exit(1)
 
     ip = "127.0.0.1"
@@ -107,4 +118,5 @@ if __name__ == "__main__":
     input_type = "bytes"
     container = TfInceptionContainer(model_checkpoint_path, gpu_mem_frac)
     rpc_service = rpc.RPCService()
-    rpc_service.start(container, ip, port, model_name, model_version, input_type)
+    rpc_service.start(container, ip, port, model_name, model_version,
+                      input_type)
