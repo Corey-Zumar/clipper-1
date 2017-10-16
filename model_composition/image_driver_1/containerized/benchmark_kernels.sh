96938c42784b45fd207864e77222fc9563819ee2
Corey Zumar <czumar@berkeley.edu>
2017-10-16 04:41:56 -0700

    reduce kernel size for tests

--- model_composition/image_driver_1/containerized/benchmark_kernels.sh
+++ model_composition/image_driver_1/containerized/benchmark_kernels.sh
@@ -11,13 +11,13 @@ unset CDPATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 END=5
-for ((i=10000;i<20000;i+=2000)); do
+for ((i=1000;i<10000;i+=2000)); do
 	echo $i
 	python $DIR/training/kernel_svm_trainer.py $i $DIR/containers/kernel_svm_model_data/kernel_svm_trained.sav
 	python $DIR/driver.py -m kernel-svm -c 17 -b 1 2 4 8 10 16 24 32 48 64
 done
 
-for ((i=20000;i<=50000;i+=5000)); do
+for ((i=10000;i<=50000;i+=5000)); do
     echo $i
    	python $DIR/training/kernel_svm_trainer.py $i $DIR/containers/kernel_svm_model_data/kernel_svm_trained.sav
 	python $DIR/driver.py -m kernel-svm -c 17 -b 1 2 4 8 10 16 24 32 48 64
