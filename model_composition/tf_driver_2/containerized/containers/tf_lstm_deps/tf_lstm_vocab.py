12d2f59cea395f51000a4cb1a1c778428bc210da
Corey Zumar <czumar@berkeley.edu>
2017-11-07 16:12:41 -0800

    Improve lstm performance

--- model_composition/tf_driver_2/containerized/containers/tf_lstm_deps/tf_lstm_vocab.py
+++ model_composition/tf_driver_2/containerized/containers/tf_lstm_deps/tf_lstm_vocab.py
@@ -18,8 +18,11 @@ class Vocabulary:
         assert os.path.exists(word_vectors_path)
 
         word_list_file = open(word_list_path, "rb")
-        self.word_list = [word.decode('UTF-8') for word in np.load(word_list_file)]
+        word_list = [word.decode('UTF-8') for word in np.load(word_list_file)]
         word_list_file.close()
+        self.word_dict = {}
+        for i in range(len(word_list)):
+            self.word_dict[word_list[i]] = i
 
         word_vectors_file = open(word_vectors_path, "rb")
         self.word_vectors = np.load(word_vectors_file)
@@ -27,8 +30,8 @@ class Vocabulary:
 
     def get_word_idx(self, word):
         try:
-            return self.word_list.index(word)
-        except ValueError:
+            return self.word_dict[word]
+        except KeyError:
             return WORD_VAL_UNKNOWN
 
     def get_word_vecs(self):
