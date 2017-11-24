67f64b8a63a0f6719ba6db9ec2de3f27e549d83e
Corey Zumar <czumar@berkeley.edu>
2017-11-24 00:41:19 -0800

    cpu alloc fix

--- model_composition/image_driver_1/containerized/driver.py
+++ model_composition/image_driver_1/containerized/driver.py
@@ -75,7 +75,7 @@ def setup_clipper(config):
         query_frontend_image="clipper/zmq_frontend:develop",
         redis_cpu_str="0",
         mgmt_cpu_str="0",
-        query_cpu_str="1-4")
+        query_cpu_str="0-5")
     time.sleep(10)
     driver_utils.setup_heavy_node(cl, config, DEFAULT_OUTPUT)
     time.sleep(10)
