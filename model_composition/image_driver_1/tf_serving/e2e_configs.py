fb08473bf87b4ebb1148dc7294c3931148f703c7
Dan Crankshaw <dscrankshaw@gmail.com>
2018-04-19 00:20:21 +0000

    fix port assignment issue in setup

--- model_composition/image_driver_1/tf_serving/e2e_configs.py
+++ model_composition/image_driver_1/tf_serving/e2e_configs.py
@@ -26,11 +26,6 @@ RESNET_152_MODEL_BASE_PATH = os.path.join(MODEL_BASE_DIR_PATH, "resnet_tfserve")
 LOG_REG_MODEL_BASE_PATH = os.path.join(MODEL_BASE_DIR_PATH, "log_reg_tfserve")
 KERNEL_SVM_MODEL_BASE_PATH = os.path.join(MODEL_BASE_DIR_PATH, "kernel_svm_tfserve")
 
-INCEPTION_PORTS = range(9500,9508)
-RESNET_152_PORTS = range(9508, 9516)
-LOG_REG_PORTS = range(9516, 9524)
-KERNEL_SVM_PORTS = range(9524, 9532)
-
 CONFIG_KEY_MODEL_NAME = "model_name"
 CONFIG_KEY_BATCH_SIZE = "batch_size"
 CONFIG_KEY_NUM_REPLICAS = "num_replicas"
@@ -40,11 +35,11 @@ CONFIG_KEY_ALLOCATED_GPUS = "allocated_gpus"
 CONFIG_KEY_HOST = "host"
 CONFIG_KEY_PORTS = "ports"
 
-def get_heavy_node_config(model_name, batch_size, num_replicas, allocated_cpus, cpus_per_replica=2, allocated_gpus=[]):
+def get_heavy_node_config(model_name, batch_size, num_replicas, ports, allocated_cpus, cpus_per_replica=2, allocated_gpus=[]):
     if model_name == INCEPTION_FEATS_MODEL_NAME:
         return tfs_utils.TFSHeavyNodeConfig(name=INCEPTION_FEATS_MODEL_NAME,
                                             model_base_path=INCEPTION_FEATS_MODEL_BASE_PATH,
-                                            ports=INCEPTION_PORTS[:num_replicas],
+                                            ports=ports,
                                             input_type="floats",
                                             allocated_cpus=allocated_cpus,
                                             cpus_per_replica=cpus_per_replica,
@@ -55,7 +50,7 @@ def get_heavy_node_config(model_name, batch_size, num_replicas, allocated_cpus, 
     elif model_name == LOG_REG_MODEL_NAME:
         return tfs_utils.TFSHeavyNodeConfig(name=LOG_REG_MODEL_NAME,
                                             model_base_path=LOG_REG_MODEL_BASE_PATH,
-                                            ports=LOG_REG_PORTS[:num_replicas],
+                                            ports=ports,
                                             input_type="floats",
                                             allocated_cpus=allocated_cpus,
                                             cpus_per_replica=cpus_per_replica,
@@ -67,7 +62,7 @@ def get_heavy_node_config(model_name, batch_size, num_replicas, allocated_cpus, 
     elif model_name == RESNET_152_MODEL_NAME:
         return tfs_utils.TFSHeavyNodeConfig(name=RESNET_152_MODEL_NAME,
                                             model_base_path=RESNET_152_MODEL_BASE_PATH,
-                                            ports=RESNET_152_PORTS[:num_replicas],
+                                            ports=ports,
                                             input_type="floats",
                                             allocated_cpus=allocated_cpus,
                                             cpus_per_replica=cpus_per_replica,
@@ -78,7 +73,7 @@ def get_heavy_node_config(model_name, batch_size, num_replicas, allocated_cpus, 
     elif model_name == KERNEL_SVM_MODEL_NAME:
         return tfs_utils.TFSHeavyNodeConfig(name=KERNEL_SVM_MODEL_NAME,
                                             model_base_path=KERNEL_SVM_MODEL_BASE_PATH,
-                                            ports=KERNEL_SVM_PORTS[:num_replicas],
+                                            ports=ports,
                                             input_type="floats",
                                             allocated_cpus=allocated_cpus,
                                             cpus_per_replica=cpus_per_replica,
@@ -99,10 +94,12 @@ def load_server_configs(configs_dir_path):
         cpus_per_replica = config_params[CONFIG_KEY_CPUS_PER_REPLICA]
         allocated_cpus = config_params[CONFIG_KEY_ALLOCATED_CPUS]
         allocated_gpus = config_params[CONFIG_KEY_ALLOCATED_GPUS]
+        ports = config_params[CONFIG_KEY_PORTS]
 
         config = get_heavy_node_config(model_name=model_name,
                                        batch_size=batch_size,
                                        num_replicas=num_replicas,
+                                       ports=ports,
                                        cpus_per_replica=cpus_per_replica,
                                        allocated_cpus=allocated_cpus,
                                        allocated_gpus=allocated_gpus)
